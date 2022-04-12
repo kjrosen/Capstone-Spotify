3220c285a77ed2c0591e8f70e5e5d068812f134b
Kaitlin Rosen <kayejrosen@gmail.com>
2022-04-11 20:36:37 -0700

    Server skeleton made

--- server.py
+++ server.py
@@ -1,2 +1,34 @@
-from flask import render_template
+from flask import Flask, render_template, request, redirect, session
+import jinja2
+import os
 
+app = Flask(__name__)
+
+@app.route('/')
+def homepage():
+    return render_template('home.html')
+
+@app.route('/join_club')
+def create_account():
+    pass
+
+@app.route('/login')
+def login():
+    pass
+
+@app.route('/search')
+def search_playlists():
+    pass
+
+@app.route('/write_new')
+def make_playlist():
+    return render_template('new_playlist.html')
+
+@app.route('/my_playlists')
+def show_user_playlists():
+    return render_template('my_playlists.html')
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0")
