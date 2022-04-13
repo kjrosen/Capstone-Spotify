93132e7b287fa9ba0333e89e41b9a6bc1efad56d
Kaitlin Rosen <kayejrosen@gmail.com>
2022-04-12 21:51:32 -0700

    Set up REACT functions

--- server.py
+++ server.py
@@ -8,22 +8,11 @@ app = Flask(__name__)
 def homepage():
     return render_template('home.html')
 
-@app.route('/join_club')
-def create_account():
-    pass
-
-@app.route('/login')
-def login():
-    pass
-
-@app.route('/search')
-def search_playlists():
-    ''''''
-    pass
-
 @app.route('/write_new')
 def make_playlist():
-    return render_template('new_playlist.html')
+    '''search through spotify api looking for songtitles fitting 
+    words from the given phrase'''
+    pass
 
 @app.route('/my_playlists')
 def show_user_playlists():
--- static/js/navbar.jsx
+++ static/js/navbar.jsx
@@ -1,5 +1,53 @@
 'use strict';
 
-function search_playlists(){
-  //make a SQL query to the playlist DB
+function MakeNavbar(){
+  //check whether the user is logged in or not
+  //if so, return a link to their playlists
+  //if not, return a form that has a log in submit option (username/password)
+  //and a create account submit option(username/password/email/spotify connection)
+  //both change the session login info but don't redirect
+  //rerendering will change what appears in this location to the my playlists link
+
+  return (
+    <span>
+      <span id="create"> 
+          New Playlist
+      </span>
+      <span id="search">
+          Search
+      </span>
+      <span id="account"></span>
+    </span>
+  )
+}//make all of those spans events in their own right
+//that render the below functions
+
+ReactDOM.render(<MakeNavbar />, document.querySelector('.navbar'));
+
+
+function Create(){
+  //render a form
+  //take the input text and send it to python to split and search through spotify
+  //render the completed playlist in .new_playlist
+
+  return(
+    <form className="popbar">
+      <input type="text" />
+      <button type="submit">Create</button>
+    </form>
+  );
+}
+
+function Search(){
+  //render a search bar
+  //perform a SQLquery for keywords searched
+  //either title or artist hit within playlists
+  //render list of playlists in .search_results
+
+  return (
+    <form className="popbar">
+      <input type="text" />
+      <button type="submit">Search</button>
+    </form>
+  );
 }
