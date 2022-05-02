d87642f9485003537882acd473ea2a29ae8f7ae6
Kaitlin Rosen <kayejrosen@gmail.com>
2022-05-02 16:05:49 -0700

    adjusted Seed.py

--- seed.py
+++ seed.py
@@ -17,11 +17,34 @@ os.system('createdb music')
 model.connect_to_db(server.app)
 model.db.create_all()
 
-## songs collected from letters and common word searches
+
+
+## takes users from users.json and fills into database
+file = open('data/users.json').read()
+all_users = json.loads(file)
+def fill_users(users=all_users):
+
+    user_list = []
+    for user_ in users:
+        new = crud.create_user(
+            users[user_][1], 
+            users[user_][2], 
+            users[user_][3],
+            users[user_][0],
+        )
+
+        user_list.append(new)
+
+
+    model.db.session.add_all(user_list)
+    model.db.session.commit()
+
+fill_users()
+
+
+## cached songs collected from letters and common word searches
 file = open('data/tracks.json').read()
-
 all_tracks = json.loads(file)
-
 def fill_tracks(tracks=all_tracks):
     '''take in a json file and seed the tracks table of db'''
 
@@ -43,50 +66,70 @@ def fill_tracks(tracks=all_tracks):
 fill_tracks()
 
 
+## makes an api search to get all tracks from a playlist, save if not in db, and create feat
+def make_feats(playlist):
 
-file = open('data/users.json').read()
-all_users = json.loads(file)
-
-def fill_users(users=all_users):
-
-    user_list = []
-    for user_ in users:
-        new = crud.create_user(
-            users[user_][1], 
-            users[user_][2], 
-            users[user_][3],
-            users[user_][0],
-        )
-
-        user_list.append(new)
+    tracks = crud.spot.playlist_items(playlist.play_id)
+    items = tracks['items']
+    feats = []
+    for item in items:
+        id = item['track']['id']
+        title = item['track']['name']
+        artist = item['track']['artists'][0]['name']
 
 
-    model.db.session.add_all(user_list)
-    model.db.session.commit()
+        if crud.Track.query.get(id) == None:
+            song = crud.create_track(id, title, artist)
+            crud.db.session.add(song)
+            crud.db.session.commit()
 
-fill_users()
+        feat = crud.create_feat(id, playlist.play_id)
+        feats.append(feat)
+
+    crud.db.session.add_all(feats)
+    crud.db.session.commit()
+
+    return feats
+
+
+## takes playlists from admin spotify account and creates tracks and feats for them
+all_plays = crud.spot2.user_playlists(crud.app_id)
+def fill_playlists_and_feats(playlists=all_plays):
+    '''fills the music db with each playlist, iterates through the results to make playlists out of them'''
+
+    items = playlists['items']
+    plays = []
+    for play in items:
+        id_ = play['id']
+        name = play['name']
+        playlist = crud.create_playlist(id_, name, 1)
+        plays.append(playlist)
+
+        make_feats(id_)
+
+    crud.db.session.add_all(plays)
+    crud.db.session.commit()
 
 
 
-file = open('data/plays.json').read()
-all_plays = json.loads(file)
+# file = open('data/plays.json').read()
+# all_plays = json.loads(file)
 
-def fill_playlists(plays=all_plays):
+# def fill_playlists(plays=all_plays):
     
-    play_list = []
-    for play in plays:
-        new = crud.create_playlist(
-            play, 
-            plays[play][0], 
-            plays[play][1],
-            plays[play][2],
-        )
+#     play_list = []
+#     for play in plays:
+#         new = crud.create_playlist(
+#             play, 
+#             plays[play][0], 
+#             plays[play][1],
+#             plays[play][2],
+#         )
 
-        play_list.append(new)
+#         play_list.append(new)
 
 
-    model.db.session.add_all(play_list)
-    model.db.session.commit()
+#     model.db.session.add_all(play_list)
+#     model.db.session.commit()
 
-fill_playlists()
-
+# fill_playlists()
